2049dfafb09447bbe23bb1c121265b4df9dd0225
mushtaq72 <mushtaq7281@gmail.com>
2023-03-24 16:08:45 +0500

    footer done

--- footer.php
+++ footer.php
@@ -1,10 +1,57 @@
+<html>
+    <head>
+<link rel="stylesheet" href="footer.css">
+    </head>
+<body>
+    <div class="container">
+        <footer class="footer">
+               <ul class="icons">
+               <nav-link><img class="socialicons" src="images/icons/Whatsapp.png"></nav-link>
+               <nav-link><img class="socialicons" src="images/icons/Youtube.png"></nav-link>
+               <nav-link><img class="socialicons" src="images/icons/Twitter.png"></nav-link>
+               <nav-link><img class="socialicons" src="images/icons/Instagram.png"></nav-link>
+               <nav-link><img class="socialicons" src="images/icons/Twitch.png"></nav-link>
+               
+              
+               </ul>
+               <div class="text">
+                <nav-link>About</nav-link>
+                <nav-link>Contact </nav-link>
+                <nav-link>Privacy</nav-link>
+                <nav-link>uses</nav-link>
+                <nav-link>Jobs</nav-link>
+                <nav-link>Terms </nav-link>
+                <nav-link>servises</nav-link>
+
+               </div>
+               
+                <h4 class="more">More from our network</h4>
+                <ul class="icons">
+                    <nav-link><img class="socialicons2" src="images/icons/WeChat.png"></nav-link>
+                    <nav-link><img class="socialicons2" src="images/icons/Viber.png"></nav-link>
+                    <nav-link><img class="socialicons2" src="images/icons/VK.png"></nav-link>
+                    <nav-link><img class="socialicons2" src="images/icons/Zoom.png"></nav-link>
+                    <nav-link><img class="socialicons2" src="images/icons/Vimeo.png"></nav-link>
+                    <nav-link><img class="socialicons2" src="images/icons/Telegram.png"></nav-link>
+                    <nav-link><img class="socialicons2" src="images/icons/Spotify.png"></nav-link>
+                    <nav-link><img class="socialicons2" src="images/icons/Snapchat.png"></nav-link>
+                    <nav-link><img class="socialicons2" src="images/icons/Slack.png"></nav-link>
+                   
+                    </ul>
+                    <h4 class="name">  Made by <strong> DANISH</strong></h4>
+
+               
+         </div>
+
+       
 
 
-    <section>
-      <div class="footer container text-center mt-5">
-        <h4>Links</h4>
-        <h4>Made by</h4>
-        <h4>etc</h4>
-      </div>
-    </section>
+
+        </footer>
+
+
+    </body>
+</html>
+
     
+   
