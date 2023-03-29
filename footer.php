9e20e8b847105eaa28e222e0815c0a6bb73abd5d
mushtaq72 <mushtaq7281@gmail.com>
2023-03-29 18:03:46 +0500

    footer

--- footer.php
+++ footer.php
@@ -1,12 +1,72 @@
+<html>
+    <head>
+<!-- <link rel="stylesheet" href="footer.css"> -->
 
-<section> 
-            
-</section>
+    </head>
+<body>
+    <footer class="footer">
+        
+            <div class="row p-5 d-flex">
+                <div class="col col-sm-12 col-md-6 col-lg-6  footer-col">
+                    <h4> catagries</h4>
+                    <ul class="navbar-nav">
+                                           
+                        <li class="nav-item"><a class="nav-link" href="#">oil</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">honey</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">milk</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">milk</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">food</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">butter</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">butter</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">butter</a></li>
+                    </ul>
+                 </div>
+
+                 <div class="col col-sm-12 col-md-6 col-lg-6  footer-col">
+                 <h4> icons</h4>
+                    <ul class="icons navbar-nav">
+                    <li class="nav-item"> <a class="nav-link" href="#"><img class="socialicons" style="width: 40px; height:40px" src="images/icons/Whatsapp.png"> </a> </li>
+                    <li class="nav-item">  <a class="nav-link" href="#"><img class="socialicons" style="width: 40px; height:40px" src="images/icons/whatsapp.png"> </a> </li>
+                    <li class="nav-item">  <a class="nav-link" href="#"><img class="socialicons" style="width: 40px; height:40px" src="images/icons/Whatsapp.png"> </a></li>
+                    <li class="nav-item">  <a class="nav-link" href="#"><img class="socialicons" style="width: 40px; height:40px" src="images/icons/Whatsapp.png"> </a></li>
+                    <li class="nav-item">  <a class="nav-link" href="#"><img class="socialicons" style="width: 40px; height:40px" src="images/icons/Whatsapp.png"> </a></li>
+                        </ul>
+                  
+                 </div>
+                
+
+
+         
+                
+            </div>
+
+        
+    </footer>
+   
+
+    </body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
-
-                <!-- <img style="height: 40px ; width: 40px;" src="images/icons/Whatsapp.png">
-               <img style="height: 40px ; width: 40px;" src="images/icons/Youtube.png">
-               <img style="height: 40px ; width: 40px;" src="images/icons/Twitter.png">
-               <img style="height: 40px ; width: 40px;" src="images/icons/Instagram.png">
-               <img style="height: 40px ; width: 40px;" src="images/icons/Twitch.png"> -->
-              
